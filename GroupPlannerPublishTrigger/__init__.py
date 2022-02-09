f9afd5fa4c5f75041a24b93d36521653e39354d4
Nick Peterson <nick.peterson@luckcompanies.com>
2022-02-09 11:34:24 -0500

    Initial Commit

---
+++ GroupPlannerPublishTrigger/__init__.py
@@ -0,0 +1,86 @@
+import logging
+from urllib import request
+
+import azure.functions as func
+import requests
+import os
+import datetime
+from pytz import timezone
+import json
+from azure.storage.filedatalake import DataLakeServiceClient
+from azure.core._match_conditions import MatchConditions
+from azure.storage.filedatalake._models import ContentSettings
+
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    groupId = req.params.get('groupId')
+    if not groupId:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            groupId = req_body.get('groupId')
+
+    tokenHeaders = {'Content-Type': 'application/x-www-form-urlencoded'}
+    tokenBody = f"grant_type=password&resource=https://graph.microsoft.com&client_id={os.getenv('ClientId')}&username={os.getenv('FunctionalAccountEmail')}&password={os.getenv('FunctionalAccountPw')}&client_secret={os.getenv('ClientSecret')}"
+    accessTokenRequest = requests.get(
+        f"https://login.microsoftonline.com/{os.getenv('TenantId')}/oauth2/token", headers=tokenHeaders, data=tokenBody)
+    accessTokenResponse = accessTokenRequest.json()
+    token = accessTokenResponse['access_token']
+    logging.info(token)
+    graphHeader = {'Authorization': f"Bearer {token}"}
+    groupNameRequest = requests.get(
+        f"https://graph.microsoft.com/v1.0/groups/{groupId}", headers=graphHeader)
+    groupNameResponse = groupNameRequest.json()
+    groupDisplayName = groupNameResponse['displayName']
+
+    groupPlansRequest = requests.get(
+        f"https://graph.microsoft.com/v1.0/groups/{groupId}/planner/plans", headers=graphHeader)
+    groupPlansResponse = groupPlansRequest.json()
+    for plan in groupPlansResponse['value']:
+
+        planDisplayName = plan['title']
+        plansTasksRequest = requests.get(
+            f"https://graph.microsoft.com/v1.0/planner/plans/{plan['id']}/tasks", headers=graphHeader)
+        fileText = json.loads(plansTasksRequest.text)
+        writeFiletoADLS(fileText, groupDisplayName, planDisplayName)
+
+    return func.HttpResponse(f"Hello, {groupId}. This HTTP triggered function executed successfully.")
+
+
+def writeFiletoADLS(jsonText, groupName, planName):
+    storage_account_name = "luckdatalakedev"
+    storage_account_key = "5TGE0TcKHT486KYamBXLRVEttwW6+S8RCtsYJWjTRsqEHLixy9T/AlMgcTmS5LauDf5DcTceDhUUZZZGORXfBQ=="
+
+    try:
+        global service_client
+
+        service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
+            "https", storage_account_name), credential=storage_account_key)
+
+    except Exception as e:
+        print(e)
+
+    file_system_client = service_client.get_file_system_client(
+        file_system="planner-data")
+
+    tz = timezone('EST')
+    now = datetime.datetime.now(tz)
+    dateOnly = now.strftime("%Y-%m-%d")
+    directory = f"{groupName}/{dateOnly}/{planName}"
+    filePath = f"{dateOnly}-{planName}.json"
+
+    file_system_client.create_directory(directory)
+    directory_client = file_system_client.get_directory_client(directory)
+    file_client = directory_client.create_file(filePath)
+    file_to_load = json.dumps(jsonText)
+    file_client.append_data(data=file_to_load, offset=0, length=len(file_to_load))
+    file_client.flush_data(len(file_to_load))
+
+    
+
+
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# PlannerDataPublisher
-Repo for CI/CD Demo
+![alt text](readmefiles\lucksmall.png)
+
+<h1 align="center">Planner Data Publisher</h1>
+
+<h2 align="left">Project Description</h2>
+
+Python Azure function for publishing Microsoft Planner data into Azure blob storage. 
+
+<h2 align="left">How to Run Locally</h2>
+
+1. Install Visual Studio Code and ensure you have the Azure Functions Extension Installed 
+2. Clone this Repository 
+3. Open folder location where you cloned repo. The .vscode project folder will import the project settings for you
+4. You will be prompted to created a Python Virtual Environment with a click of a button for project to run on
+5. From the tool bar Run -> Start Debugging will run the function locally 
+
+Note that this function app requires a functional account due to the Planner API requiring [Delegated Permissions](https://docs.microsoft.com/en-us/graph/auth/auth-concepts#delegated-and-application-permissions). It also requires a Blob Storage & Access Key to land Planner Data into. 
+
+All of these configuration keys/secrets are set inside of local.settings.json
+
+
+<h2 align="left">How to Contribute</h2>
+
+To contribute to this Repository branch off of Main and name the branch to include your name and short description of work you are doing. Once your work has been completed you may begin a merge request into Main. 
+
+
+<h2 align="left">Built With</h2>
+
+- Python 3.7
+- Visual Studio Code 
+- Azure Functions 
+
+Python Packages
+
+- requests 
+- azure-storage-file-datalake
+- pytz
+
+
+<h2 align="left">Author</h2>
+
+Nicholas Peterson 
+
+Data Engineer  :sunglasses:
+
+Nick.Peterson@luckcompanies.com  :four_leaf_clover:
